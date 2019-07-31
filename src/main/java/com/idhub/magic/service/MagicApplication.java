9af3508ebb82fca6206b732bb87e16b054863d0e
baiyuqi <46093894+baiyuqi@users.noreply.github.com>
2019-08-01 06:11:11 +0800

    Add files via upload

---
+++ src/main/java/com/idhub/magic/service/MagicApplication.java
@@ -0,0 +1,17 @@
+
+package com.idhub.magic.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication
+public class MagicApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(MagicApplication.class, args);
+
+		
+	}
+
+}
