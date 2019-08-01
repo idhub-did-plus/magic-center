53a5e39d1045c9f77a6dad2ba1558342666bba70
baiboshi <yuqi.bai@albus.com.cn>
2019-08-01 17:13:29 +0800

    dd

--- src/main/java/com/idhub/magic/center/service/ContractManager.java
+++ src/main/java/com/idhub/magic/center/service/ContractManager.java
@@ -70,6 +70,7 @@ public class ContractManager {
                 credentials,
                 contractGasProvider
         );
+    
     }
     
      public EthereumClaimsRegistryInterface getRegistry780() {
