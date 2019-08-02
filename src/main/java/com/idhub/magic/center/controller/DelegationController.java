91be9f55551ba101483ad457d1bdb826140ca535
baiboshi <yuqi.bai@albus.com.cn>
2019-08-02 19:02:18 +0800

    dd

--- src/main/java/com/idhub/magic/center/controller/DelegationController.java
+++ src/main/java/com/idhub/magic/center/controller/DelegationController.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.web3j.abi.Utils;
 import com.alibaba.fastjson.JSON;
 import com.idhub.magic.center.controller.parameter.CreateIdentityDelegatedParam;
 import com.idhub.magic.center.service.DelegationService;
-import com.idhub.magic.center.util.AuthenticationUtils;
 
 @RestController
 @RequestMapping("/delegation")
--- src/test/java/com/idhub/magic/center/ClientTest.java
+++ src/test/java/com/idhub/magic/center/ClientTest.java
@@ -1,6 +1,14 @@
 package com.idhub.magic.center;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Uint;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.generated.Uint64;
+import org.web3j.crypto.ContractUtils;
+import org.web3j.crypto.Hash;
+import org.web3j.utils.Numeric;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -8,16 +16,19 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.assertj.core.util.Preconditions;
 
 import com.alibaba.fastjson.JSON;
+import com.idhub.magic.center.controller.parameter.CreateIdentityDelegatedParam;
 import com.idhub.magic.center.crypto.CryptoManager;
+import com.idhub.magic.center.service.DeployedContractAddress;
 import com.idhub.magic.center.util.AuthenticationUtils;
+import com.idhub.magic.center.util.CryptoUtil;
 import com.idhub.magic.center.util.Signature;
-
 public class ClientTest {
-
-	public static void main(String[] args) {
 	
+	public static void main(String[] args) {
+
 		  long ts = System.currentTimeMillis();
 		  String id = CryptoManager.getCredentials().getAddress();
 		  String pt = id + ts;
@@ -40,6 +51,19 @@ public class ClientTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	
+		   byte[] permissionString = CryptoUtil.soliditySha3(
+			        "0x19", "0x00", DeployedContractAddress.IdentityRegistryInterface,
+			        "I authorize the creation of an Identity on my behalf.",
+			        null,
+			        null,
+			        null, null, null
+			       
+			      );
+		 
+		  CreateIdentityDelegatedParam p = new CreateIdentityDelegatedParam();
+		  
+		  
 		  
 	      
 
