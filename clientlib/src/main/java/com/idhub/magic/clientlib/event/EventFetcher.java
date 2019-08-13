113c06974937102646a69079d0093ba39fe9c3c9
baiboshi <yuqi.bai@albus.com.cn>
2019-08-13 13:28:36 +0800

    hhh

--- clientlib/src/main/java/com/idhub/magic/clientlib/event/EventFetcher.java
+++ clientlib/src/main/java/com/idhub/magic/clientlib/event/EventFetcher.java
@@ -1,5 +1,8 @@
 package com.idhub.magic.clientlib.event;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,28 +13,46 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.IOUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.idhub.magic.center.event.ChainEvent;
+import com.idhub.magic.clientlib.ProviderFactory;
+import com.idhub.magic.clientlib.http.HttpAccessor;
 
 public class EventFetcher {
 	static EventFetcher instance = new EventFetcher();
+
 	static public EventFetcher getInstance() {
 		return instance;
 	}
-	 Map<String, EventListener> listeners = new HashMap<String, EventListener>();
-	 ScheduledExecutorService pool;
-	 private EventFetcher()
-	 {
+
+	static String url = "http://localhost:8000/chainevent/getChianEvents";
+	Map<String, EventListener> listeners = new HashMap<String, EventListener>();
+	ScheduledExecutorService pool;
+
+	private EventFetcher() {
+		final String identity = ProviderFactory.getProvider().getDefaultCredentials().getAddress();
 		pool = Executors.newScheduledThreadPool(1);
 		pool.scheduleAtFixedRate(() -> {
-			
-			
-			
-			
+
+			String json = HttpAccessor.get(identity, url, null);
+			JSONObject data = (JSONObject) JSON.parse(json);
+			Boolean suc = (Boolean) data.get("success");
+			if (!suc)
+				return;
+			Object esdata = data.get("data");
+			String ess = esdata.toString();
+			List<ChainEvent> rst = JSON.parseArray(ess, ChainEvent.class);
+			for (ChainEvent e : rst)
+				processEvent(e);
+
 		}, 0, 15, TimeUnit.SECONDS);
 
 	}
 
-	 void processEvent(ChainEvent e) {
+	void processEvent(ChainEvent e) {
 
 		Map<String, EventListener> temp = new HashMap<String, EventListener>(listeners);
 		for (String t : temp.keySet()) {
@@ -43,9 +64,16 @@ public class EventFetcher {
 		}
 	}
 
-	 public void listen(EventListener l) {
+	public void listen(EventListener l) {
 		String threadId = UUID.randomUUID().toString();
 		listeners.put(threadId, l);
+	}
+
+	static public void main(String[] ss) throws Exception {
+		JSONObject data = (JSONObject) JSON.parse(IOUtils.toString(new FileInputStream("new-filter.json")));
+		Boolean suc = (Boolean) data.get("success");
+		if (!suc)
+			System.out.println(data);
 	}
 
 }
--- clientlib/src/main/java/com/idhub/magic/clientlib/http/HttpAccessor.java
+++ clientlib/src/main/java/com/idhub/magic/clientlib/http/HttpAccessor.java
@@ -1,12 +1,24 @@
 package com.idhub.magic.clientlib.http;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.web3j.crypto.Credentials;
 
 import com.alibaba.fastjson.JSON;
@@ -15,10 +27,39 @@ import com.idhub.magic.center.util.Signature;
 import com.idhub.magic.clientlib.ProviderFactory;
 
 public class HttpAccessor {
-	static public <T> T get(String identity, String url, Map<String, String> params){
-		return null;
+	static public String get(String identity, String url, Map<String, String> params) {
+		long ts = System.currentTimeMillis();
+		String sgt = authenticate(identity, ts);
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("identity", identity));
+		nvps.add(new BasicNameValuePair("timestamp", ts + ""));
+		if (params != null) {
+
+			for (String key : params.keySet()) {
+				String k = key;
+
+				nvps.add(new BasicNameValuePair(k, params.get(key)));
+			}
+
+		}
+		HttpGet request = new HttpGet(url);
+		request.addHeader("signature", sgt);
+		try {
+			URI uri = new URIBuilder(request.getURI()).addParameters(nvps).build();
+			request.setURI(uri);
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			CloseableHttpResponse resp = httpClient.execute(request);
+			String json = EntityUtils.toString(resp.getEntity());
+			return json;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
 	}
-	static public <T> T post(String identity, String url, String body){
+
+	static public String post(String identity, String url, String body) {
 		long ts = System.currentTimeMillis();
 		String sgt = authenticate(identity, ts);
 		HttpPost post = new HttpPost(url + "?identity=" + identity + "&timestamp=" + ts);
@@ -29,20 +70,23 @@ public class HttpAccessor {
 			post.setEntity(en);
 			CloseableHttpClient httpClient = HttpClients.createDefault();
 			CloseableHttpResponse resp = httpClient.execute(post);
+			String json = EntityUtils.toString(resp.getEntity());
+			return json;
 		} catch (Exception e) {
 			e.printStackTrace();
-			
-		} 
+
+		}
 		return null;
-		
+
 	}
+
 	static private String authenticate(String identity, long ts) {
 		String pt = identity + ts;
 		Credentials cre = ProviderFactory.getProvider().getByAddress(identity);
-		Signature sig = AuthenticationUtils.sig(pt,cre);
+		Signature sig = AuthenticationUtils.sig(pt, cre);
 		String sgt = JSON.toJSONString(sig);
 		boolean authenticated = AuthenticationUtils.authenticate(sgt, identity + ts, identity);
 		return sgt;
 	}
-	
+
 }
