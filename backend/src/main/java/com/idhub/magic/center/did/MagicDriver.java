1e4cb597e450deb1064153f65d0b768f4e34acfb
baiyuqi <yuqi.bai8888@gmail.com>
2019-08-21 06:31:48 +0800

    mm

---
+++ backend/src/main/java/com/idhub/magic/center/did/MagicDriver.java
@@ -0,0 +1,23 @@
+package com.idhub.magic.center.did;
+
+import java.util.Map;
+
+import com.idhub.magic.did.driver.Driver;
+import com.idhub.magic.did.resolver.ResolutionException;
+import com.idhub.magic.did.resolver.ResolveResult;
+
+public class MagicDriver implements Driver {
+
+	@Override
+	public ResolveResult resolve(String identifier) throws ResolutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> properties() throws ResolutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
