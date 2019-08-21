408a5bbf2d50c2d768ac9eefd85786339eb5699f
baiyuqi <1353813001@qq.com>
2019-08-21 18:19:16 +0800

    ff

--- clientlib/src/main/java/com/idhub/magic/clientlib/interfaces/IdentityStorage.java
+++ clientlib/src/main/java/com/idhub/magic/clientlib/interfaces/IdentityStorage.java
@@ -3,11 +3,25 @@ package com.idhub.magic.clientlib.interfaces;
 import com.idhub.magic.center.parameter.MagicResponse;
 import com.idhub.magic.center.ustorage.entity.IdentityArchive;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
 
 public interface IdentityStorage {
-	void storeArchive(IdentityArchive archive);
-	 @GET("retrieve_archive?identity={identity}")
-	 Call<MagicResponse<IdentityArchive>>  retrieveArchive(String identity);
+	 @POST("/storeArchive")
+     @FormUrlEncoded
+     Call<MagicResponse> storeArchive(IdentityArchive archive, @Query("cate")String identity);
+	 @GET("retrieve_archive")
+	 Call<MagicResponse<IdentityArchive>>  retrieveArchive(@Query("cate")String identity);
+
+     @POST("/load_materiale")
+     @Multipart
+     Call<MagicResponse> uploadMaterial(@Query("cate")String identity, @Part("name") RequestBody name, @Part("type") RequestBody age, @Part MultipartBody.Part file);
+
 }
