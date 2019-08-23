bcaabc4524293a1b9980b65061eefe74c27c5b3b
baiyuqi <yuqi.bai8888@gmail.com>
2019-08-24 07:41:22 +0800

    mm

--- backend/src/main/java/com/idhub/magic/center/account/AccountManager.java
+++ backend/src/main/java/com/idhub/magic/center/account/AccountManager.java
@@ -1,5 +1,6 @@
 package com.idhub.magic.center.account;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.slf4j.Logger;
@@ -15,8 +16,8 @@ public class AccountManager {
 	static Credentials client;
 	static {
 		try {
-			
-			credentials = WalletUtils.loadCredentials("2218283", "c:\\mspace\\magic-center\\backend\\keystore\\keys.json");
+			File source = new File("keystore\\keys.json");
+			credentials = WalletUtils.loadCredentials("2218283", source);
 			server = Credentials.create("ac913fcc48c3524e995a5b1a30b8ab8dbee69091fd72e65b1b0afcc4575eb9c4");
 			client = Credentials.create("0f2e67d493a271e2421929cb56f58bce05b27c081f18ab1b9491b4394e0116a2");
 			log.info("Credentials loaded");
