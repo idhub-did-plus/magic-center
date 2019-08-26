1563bd86845d97f328ae55414752d4166c0141ad
baiyuqi <1353813001@qq.com>
2019-08-26 13:41:04 +0800

    mm

---
+++ backend/src/main/java/com/idhub/magic/center/entity/KycOrderWrapper.java
@@ -0,0 +1,9 @@
+package com.idhub.magic.center.entity;
+
+import com.idhub.magic.center.kvc.entity.KycOrder;
+
+public class KycOrderWrapper {
+	String id;
+	String state;
+	KycOrder order;
+}
---
+++ backend/src/main/java/com/idhub/magic/center/service/KycOrderService.java
@@ -0,0 +1,14 @@
+package com.idhub.magic.center.service;
+
+import org.springframework.stereotype.Service;
+
+import com.idhub.magic.center.kvc.entity.KycOrder;
+
+@Service
+public class KycOrderService {
+	
+	public void store(KycOrder order) {
+		
+	}
+
+}
