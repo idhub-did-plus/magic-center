6ec05a067d68f04fb3805d5a90e295a3804dfe2b
baiyuqi <1353813001@qq.com>
2019-08-30 15:05:04 +0800

    pp

--- provider/src/test/java/com/idhub/magic/provider/test/CustomerTest.java
+++ provider/src/test/java/com/idhub/magic/provider/test/CustomerTest.java
@@ -18,7 +18,7 @@ import okhttp3.ResponseBody;
 
 public class CustomerTest {
 
-	@Test
+
 	public void test() throws Exception {
 		IdentityMindProvider provider = new IdentityMindProvider();
 		provider.init();
@@ -29,7 +29,15 @@ public class CustomerTest {
 		
 	}
 	
-	
+	@Test
+	public void testState() throws Exception {
+		IdentityMindProvider provider = new IdentityMindProvider();
+		provider.init();
+		ConsumerService service = provider.getCustomerService();
+		ConsumerKycResponse resp = service.retrievestate("9d647da0a5ec48d4a59aebcd50876289").execute().body();
+		System.out.println(resp);
+		
+	}
 	
 	static public void main(String[] ss) throws Throwable {
 		String json = "{\"user\":\"UNKNOWN\",\"upr\":\"UNKNOWN\",\"ednaScoreCard\":{\"sc\":[{\"test\":\"ed:1\",\"details\":\"true\"}],\"etr\":[{\"test\":\"ed:20\",\"details\":\"true\"},{\"test\":\"ed:31\",\"details\":\"true\"},{\"test\":\"ed:2\",\"details\":\"true\"},{\"test\":\"ed:1\",\"details\":\"true\"},{\"test\":\"ed:28\",\"details\":\"true\"}],\"er\":{\"profile\":\"DEFAULT\",\"reportedRule\":{\"name\":\"Sandbox Rule\",\"details\":\"[Fired] details\",\"description\":\"Rule fired from sandbox\",\"ruleId\":1000,\"testResults\":[{\"test\":\"ed:1\",\"fired\":true,\"details\":\"[Fired] details\",\"condition\":{\"left\":\"ed:1\",\"right\":true,\"operator\":\"eq\"},\"ts\":1567138039000,\"stage\":\"1\"}],\"resultCode\":\"ACCEPT\"}}},\"frn\":\"Sandbox Rule\",\"frp\":\"ACCEPT\",\"frd\":\"[Fired] details\",\"mtid\":\"9d647da0a5ec48d4a59aebcd50876289\",\"state\":\"A\",\"tid\":\"9d647da0a5ec48d4a59aebcd50876289\",\"erd\":\"Unknown User\",\"res\":\"ACCEPT\",\"rcd\":\"131,100,50005,120,1000,153,202\"}";
