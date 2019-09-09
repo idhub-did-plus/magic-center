150245fe3837261d6b926866c138e0c6f8d29bf2
baiyuqi <1353813001@qq.com>
2019-09-09 10:30:49 +0800

    fff

--- provider/src/main/java/com/idhub/magic/provider/agent/OrderBookAgent.java
+++ provider/src/main/java/com/idhub/magic/provider/agent/OrderBookAgent.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import com.idhub.magic.provider.Order;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface OrderBookAgent{
-	
-	public List<Order> tome(String providerIdentity) ;
+	@GET("orderbook/directed")
+	public  Call<List<Order>> directed(@Query("identity") String providerIdentity) ;
+	@GET("orderbook/receive")
+	public boolean receive(@Query("identity") String providerIdentity, String orderId);
+	@GET("orderbook/list-all")
+	public List<Order> listAll(@Query("identity") String providerIdentity);
 
-	public boolean receive(String orderId);
-
-	public List<Order> listAll();
 }
