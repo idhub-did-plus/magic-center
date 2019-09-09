3bb17b42d483a6defd06fa4eaa19331630821f22
baiyuqi <1353813001@qq.com>
2019-09-09 11:54:16 +0800

    fff

---
+++ backend/src/main/java/com/idhub/magic/center/service/VerifiableCredentialService.java
@@ -0,0 +1,10 @@
+package com.idhub.magic.center.service;
+
+import com.idhub.magic.verifiablecredentials.VerifiableCredential;
+
+public class VerifiableCredentialService {
+	public void store(VerifiableCredential vc) {
+		
+	}
+
+}
