0fd9b35a40776cdaca3daf2aa16a54e56176ad34
baiyuqi <1353813001@qq.com>
2019-09-16 15:11:18 +0800

    dd

--- provider/src/main/java/com/idhub/magic/provider/service/IdentityEntity.java
+++ provider/src/main/java/com/idhub/magic/provider/service/IdentityEntity.java
@@ -5,28 +5,28 @@ import org.mongodb.morphia.annotations.Id;
 import com.idhub.magic.provider.IdentityData;
 
 public class IdentityEntity{
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public IdentityData getData() {
-		return data;
-	}
-	public void setData(IdentityData data) {
-		this.data = data;
-	}
 	@Id
 	String id;
 	IdentityData data;
+	public IdentityEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	public IdentityEntity(IdentityData data) {
 		super();
 		this.data = data;
 		id = data.getIdentity();
 	}
-	public IdentityEntity() {
-		super();
-		// TODO Auto-generated constructor stub
+	public IdentityData getData() {
+		return data;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setData(IdentityData data) {
+		this.data = data;
+	}
+	public void setId(String id) {
+		this.id = id;
 	}
 }
