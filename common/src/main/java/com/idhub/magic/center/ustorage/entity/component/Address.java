a05334590b5213cbd0981f359864ef7a47363212
baiyuqi <yuqi.bai8888@gmail.com>
2019-09-17 07:20:32 +0800

    ff

--- common/src/main/java/com/idhub/magic/center/ustorage/entity/component/Address.java
+++ common/src/main/java/com/idhub/magic/center/ustorage/entity/component/Address.java
@@ -1,46 +1,25 @@
 package com.idhub.magic.center.ustorage.entity.component;
 
+import java.util.List;
+
 public class Address {
-	String state;//province
-	String city;
-	String neighborhood;
-	String detailAddress;
-	Street street;
+	List<AddressElement> addressSequence;
 	String postalCode;
-	public String getCity() {
-		return city;
-	}
-	public String getDetailAddress() {
-		return detailAddress;
-	}
-	public String getNeighborhood() {
-		return neighborhood;
-	}
+
 	public String getPostalCode() {
 		return postalCode;
 	}
-	public String getState() {
-		return state;
-	}
-	public Street getStreet() {
-		return street;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public void setDetailAddress(String detailAddress) {
-		this.detailAddress = detailAddress;
-	}
-	public void setNeighborhood(String neighborhood) {
-		this.neighborhood = neighborhood;
-	}
+	
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
-	public void setState(String state) {
-		this.state = state;
+
+	public List<AddressElement> getAddressSequence() {
+		return addressSequence;
 	}
-	public void setStreet(Street street) {
-		this.street = street;
+
+	public void setAddressSequence(List<AddressElement> addressSequence) {
+		this.addressSequence = addressSequence;
 	}
+	
 }
---
+++ common/src/main/java/com/idhub/magic/center/ustorage/entity/component/AddressTemplate.java
@@ -0,0 +1,28 @@
+package com.idhub.magic.center.ustorage.entity.component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AddressTemplate {
+	static Map<String, List<AddressElement>> templates = new HashMap<String, List<AddressElement>>();
+	static {
+		templates.put("default", create("state","city","neighbourhood","streetNumber", "houseNumber", "apartmentNumber"));
+		templates.put("us", create("state","city","neighbourhood","streetNumber", "houseNumber", "apartmentNumber"));
+		templates.put("us", create("province","city","county","street", "residence","menpaihao"));
+	}
+	static public List<AddressElement> template(String country){
+		List<AddressElement> temp = templates.get(country);
+		if(temp != null)
+			return temp;
+		return templates.get("default");
+	}
+	static List<AddressElement> create(String... names){
+		List<AddressElement> rst = new ArrayList<AddressElement>();
+		for(String name : names) {
+			rst.add(new AddressElement(name));
+		}
+		return rst;
+	}
+}
