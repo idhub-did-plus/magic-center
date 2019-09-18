e67ed2e9bbafede19e3bb6bdddcfd508fc0f24a2
baiyuqi <yuqi.bai8888@gmail.com>
2019-09-18 08:24:58 +0800

    ss

--- provider/src/main/java/com/idhub/magic/provider/controller/IdentityMindController.java
+++ provider/src/main/java/com/idhub/magic/provider/controller/IdentityMindController.java
@@ -1,25 +1,13 @@
 package com.idhub.magic.provider.controller;
 
-import java.io.IOException;
-
 import org.mongodb.morphia.Datastore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.idhub.magic.center.kvc.entity.ClaimOrder;
 import com.idhub.magic.center.parameter.MagicResponse;
-import com.idhub.magic.provider.Order;
-import com.idhub.magic.provider.kyc.idmind.ConsumerService;
-import com.idhub.magic.provider.kyc.idmind.Converter;
-import com.idhub.magic.provider.kyc.idmind.IdentityMindProvider;
 import com.idhub.magic.provider.kyc.idmind.Interaction;
-import com.idhub.magic.provider.kyc.idmind.entity.consumer.ConsumerKycRequest;
-import com.idhub.magic.provider.kyc.idmind.entity.consumer.ConsumerKycResponse;
-import com.idhub.magic.provider.model.IdentityEntity;
 import com.idhub.magic.provider.service.IdentityMindService;
 
 
