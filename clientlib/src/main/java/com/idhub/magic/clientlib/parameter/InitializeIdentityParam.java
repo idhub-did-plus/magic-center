685586fb007276f0d0370bb0d5214889dffc5e49
lyw <liyuwei2010@foxmail.com>
2019-09-19 17:27:55 +0800

    add clientlib contractApi

---
+++ clientlib/src/main/java/com/idhub/magic/clientlib/parameter/InitializeIdentityParam.java
@@ -0,0 +1,4 @@
+package com.idhub.magic.clientlib.parameter;
+
+public class InitializeIdentityParam {
+}
--- common/src/main/java/com/idhub/magic/center/service/DeployedContractAddress.java
+++ common/src/main/java/com/idhub/magic/center/service/DeployedContractAddress.java
@@ -1,8 +1,8 @@
 package com.idhub.magic.center.service;
 
 public interface DeployedContractAddress {
-	static public  final String IdentityRegistryInterface = "0xcba0524dd4391588091E57A64F6d145D92016e6F";
-	static public  final String ERC1056ResolverInterface = "0x1D2EC6c935150139E736FB43F1C96788aab2A8E3";
-	static public  final String EthereumDIDRegistryInterface = "0xba3Cc1Ab60fB9Eb9D3D3149b03dbCc8050383FBb";
+	static public  final String IdentityRegistryInterface = "0x90e1B1C7B8C829b3d0b1C09eD961e46f5AeeD184";
+	static public  final String ERC1056ResolverInterface = "0x6d0f04B6Ca0217323af7fB7147a63C97Ef910617";
+	static public  final String EthereumDIDRegistryInterface = "0x42197FC11ad0765567e1b552d4063464DE938923";
 	static public  final String EthereumClaimsRegistryInterface = "0xaC7A17ecBab2694FB0b3dE4a93dA76be99E58604";
 }
