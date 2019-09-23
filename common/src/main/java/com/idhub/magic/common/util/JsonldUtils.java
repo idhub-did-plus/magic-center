03df7f52ef872d63b0616d095a161c8770744439
baiyuqi <1353813001@qq.com>
2019-09-23 19:23:52 +0800

    dd

---
+++ common/src/main/java/com/idhub/magic/common/util/JsonldUtils.java
@@ -0,0 +1,63 @@
+package com.idhub.magic.common.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.ASN1StreamParser;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.DERSequenceGenerator;
+
+public class JsonldUtils {
+	 static  protected String encodeSignature(byte[] r, byte[] s, byte[] v) {
+         try {
+			// Usually 70-72 bytes.
+			  ByteArrayOutputStream bos = new ByteArrayOutputStream(72);
+			  DERSequenceGenerator seq = new DERSequenceGenerator(bos);
+			  seq.addObject(new ASN1Integer(r));
+			  seq.addObject(new ASN1Integer(s));
+			  seq.addObject(new ASN1Integer(v));
+			  seq.close();
+			  String rst = Base64.getEncoder().encodeToString(bos.toByteArray());
+			  return rst;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+     }
+	 static public List<byte[]> decodeSignature(String signature) {
+		 byte[] data = Base64.getDecoder().decode(signature);
+			try {
+				List<byte[]> rst = new ArrayList<byte[]>();
+				 ASN1StreamParser parser = new ASN1StreamParser(data);
+				 ASN1Encodable hh = parser.readObject();
+				 DERSequence p = (DERSequence) hh.toASN1Primitive();
+				 ASN1Encodable[] as = p.toArray();
+				 for(ASN1Encodable i  : as) {
+					 ASN1Integer ii = (ASN1Integer)i;
+					byte[] s =  ii.getEncoded("DER");
+					
+					byte[] e = get(s);
+					rst.add(s);
+				 }
+				 return rst;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+		static byte[] get(byte[] data) {
+			int length = data[1];
+			byte[] rst = new byte[length];
+			for(int i = 0; i < length; i++)
+				rst[i] = data[i + 2];
+			return rst;
+		}
+
+}
