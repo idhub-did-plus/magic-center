247fe40c68d3b3924e8ccd6b5cdf710ce0493a41
baiyuqi <1353813001@qq.com>
2019-09-23 14:23:14 +0800

    dd

--- common/src/main/java/com/idhub/magic/common/ustorage/entity/FinancialProfile.java
+++ common/src/main/java/com/idhub/magic/common/ustorage/entity/FinancialProfile.java
@@ -1,7 +1,13 @@
 package com.idhub.magic.common.ustorage.entity;
 
 public class FinancialProfile {
-	public String buerType;
+	public String buyerType;
 	public String investorType;
 
+}
+enum BuyerType{
+	bt1,bt2,bt3,bt4
+}
+enum InvestorType{
+	it1,it2
 }
