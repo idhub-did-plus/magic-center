7f7d56a6afebdc71f8de0056842844a03350d748
baiyuqi <1353813001@qq.com>
2019-09-24 12:40:56 +0800

    kk

---
+++ backend/src/main/java/com/idhub/magic/center/service/IdentityAggregationService.java
@@ -0,0 +1,37 @@
+package com.idhub.magic.center.service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.tuples.generated.Tuple4;
+
+@Service
+public class IdentityAggregationService {
+	@Autowired ContractManager manager;
+	public boolean validate(String addr1, String addr2) {
+		try {
+			boolean same =  addr1.equals(addr2);
+			if(same)
+				return true;
+			BigInteger ein = manager.registry1484.getEIN(addr1).send();
+			if(ein.longValue() <= 0)
+				return false;
+			
+			Tuple4<String, List<String>, List<String>, List<String>> identity = manager.registry1484.getIdentity(ein).send();
+			
+			
+			return identity.getValue2().contains(addr2);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+		
+		
+		
+	}
+	
+
+}
