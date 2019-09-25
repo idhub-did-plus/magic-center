63d8116a878740dddc68a40b4438b2b4f0e5ee13
baiyuqi <1353813001@qq.com>
2019-09-25 12:03:11 +0800

    dd

--- provider/src/main/java/com/idhub/magic/provider/kyc/idmind/Converter.java
+++ provider/src/main/java/com/idhub/magic/provider/kyc/idmind/Converter.java
@@ -21,22 +21,7 @@ public class Converter {
 		if(name != null) {
 			req.bfn = name.getFirstName();
 			req.bln = name.getLastName();
-		}else {
-			req.man = "jdoe";
-			req.soc = "facebook";
-			req.tea = "jdoe1964@gmail.com";
-			req.bc = "Jersey City";
-			req.bco = "US";
-			req.bfn = "John";
-			req.bln = "Doe";
-			req.bz = "07304";
-			req.bs = "NJ";
-			req.bsn = "123 1st St";
-			req.dft = "AU";
-			req.dfp = "187ASDSA&#EAD129E…";
-			req.ip = "72.160.112.466";
-			req.pbc = "89756415ac…";
-			req.ptoken = "4697XXXXXX000e";
+		
 		}
 		req.bco = archive.getIdentityInfo().getCountry();
 		
