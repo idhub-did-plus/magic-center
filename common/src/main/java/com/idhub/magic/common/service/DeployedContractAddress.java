e25d4e018c8af377c1d266bab0fba58e7a743fb5
baiyuqi <1353813001@qq.com>
2019-09-26 15:12:25 +0800

    ll

--- common/src/main/java/com/idhub/magic/common/service/DeployedContractAddress.java
+++ common/src/main/java/com/idhub/magic/common/service/DeployedContractAddress.java
@@ -14,7 +14,7 @@ public class DeployedContractAddress {
 	Addresses rospen = new Addresses(
 			new String[] { "0x90e1B1C7B8C829b3d0b1C09eD961e46f5AeeD184", "0x6d0f04B6Ca0217323af7fB7147a63C97Ef910617",
 					"0x42197FC11ad0765567e1b552d4063464DE938923", "0x8940D955F437d592c764cA243aeB7742BC156F9C" });
-	Addresses current = main;
+	Addresses current = rospen;
 
 	public String addr1484() {
 		return current.addr1484();
