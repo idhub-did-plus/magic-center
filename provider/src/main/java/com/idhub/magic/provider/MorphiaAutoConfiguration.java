12e7e13789601f892aece3ba922616681b531e6b
baiyuqi <1353813001@qq.com>
2019-09-27 16:26:55 +0800

    rr

--- provider/src/main/java/com/idhub/magic/provider/MorphiaAutoConfiguration.java
+++ provider/src/main/java/com/idhub/magic/provider/MorphiaAutoConfiguration.java
@@ -1,10 +1,13 @@
 package com.idhub.magic.provider;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.annotations.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
@@ -15,25 +18,36 @@ import com.mongodb.MongoClient;
 @Configuration
 public class MorphiaAutoConfiguration {
 
-    @Autowired
-    private MongoClient mongoClient; // created from MongoAutoConfiguration
+	@Autowired
+	private MongoClient mongoClient; // created from MongoAutoConfiguration
 
-    @Bean
-    public Datastore datastore() {
-        Morphia morphia = new Morphia();
+	@Bean
+	public Datastore datastore() {
+		Morphia morphia = new Morphia();
 
-        // map entities, there is maybe a better way to find and map all entities
-        ClassPathScanningCandidateComponentProvider entityScanner = new ClassPathScanningCandidateComponentProvider(true);
-        entityScanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
-        for (BeanDefinition candidate : entityScanner.findCandidateComponents("com.idhub.magic.center.entity")) { // from properties?
-            try {
+		// map entities, there is maybe a better way to find and map all entities
+		ClassPathScanningCandidateComponentProvider entityScanner = new ClassPathScanningCandidateComponentProvider(
+				true);
+		entityScanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
+		for (BeanDefinition candidate : entityScanner.findCandidateComponents("com.idhub.magic.center.entity")) { // from
+																													// properties?
+			try {
 				morphia.map(Class.forName(candidate.getBeanClassName()));
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
+		}
 
-        return morphia.createDatastore(mongoClient, "dataStoreInstanceId"); // "dataStoreInstanceId" may come from properties?
-    }
+		return morphia.createDatastore(mongoClient, "dataStoreInstanceId"); // "dataStoreInstanceId" may come from
+																			// properties?
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("20480KB");
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
+	}
 }
