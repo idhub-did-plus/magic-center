3174ca75500e156b8d1ddbd2a4aab5019ab2b086
baiyuqi <1353813001@qq.com>
2019-10-09 12:20:57 +0800

    yy

--- common/src/main/java/com/idhub/magic/common/claim/ClaimSeed.java
+++ common/src/main/java/com/idhub/magic/common/claim/ClaimSeed.java
@@ -1,8 +1,32 @@
 package com.idhub.magic.common.claim;
 
 public class ClaimSeed {
-	String thirpartyAddress;
-	
-	
-
+	String identity;
+	String thirpartyIdentity;
+	String identityArchiveJson;
+	String signatureHex;
+	public String getIdentity() {
+		return identity;
+	}
+	public String getIdentityArchiveJson() {
+		return identityArchiveJson;
+	}
+	public String getSignatureHex() {
+		return signatureHex;
+	}
+	public String getThirpartyIdentity() {
+		return thirpartyIdentity;
+	}
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+	public void setIdentityArchiveJson(String identityArchiveJson) {
+		this.identityArchiveJson = identityArchiveJson;
+	}
+	public void setSignatureHex(String signatureHex) {
+		this.signatureHex = signatureHex;
+	}
+	public void setThirpartyIdentity(String thirpartyIdentity) {
+		this.thirpartyIdentity = thirpartyIdentity;
+	}
 }
