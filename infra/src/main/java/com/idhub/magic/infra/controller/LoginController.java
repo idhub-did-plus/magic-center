ca1ae8d510a26673b675168316318b8092818d9a
baiyuqi <yuqi.bai8888@gmail.com>
2020-05-21 11:18:16 +0800

    hh

--- infra/src/main/java/com/idhub/magic/infra/controller/LoginController.java
+++ infra/src/main/java/com/idhub/magic/infra/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.idhub.magic.infra.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.mongodb.morphia.Datastore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.idhub.magic.infra.access.SignatureToken;
 
 @CrossOrigin
 @RestController
@@ -15,7 +19,18 @@ public class LoginController {
     @Autowired Datastore datastore;
     @GetMapping("/login")
 	public MagicResponse login(String action, String identity, String timestamp, String claim, String signature) {
-		return new MagicResponse();
+	
+    	
+     Subject sub = SecurityUtils.getSubject();
+        
+        
+
+        SignatureToken token = new SignatureToken(identity, timestamp + identity  ,signature);
+          
+        sub.login(token);
+      //  SecurityUtils.getSubject().checkRole("lawer");
+        return new MagicResponse();
+
 	}
     @GetMapping("/logout")
    	public MagicResponse logout() {
